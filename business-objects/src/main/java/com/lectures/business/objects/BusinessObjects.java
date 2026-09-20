f4dcf0588f6f948c9b1edafa002198c520e6d148
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2026-09-20 15:56:18 +0300

    Init a project

---
+++ business-objects/src/main/java/com/lectures/business/objects/BusinessObjects.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.lectures.business.objects;
+
+/**
+ *
+ * @author burak
+ */
+public class BusinessObjects {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
