164bf64919d4d4bb3090e1ee21d08961d23093e0
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2026-09-20 16:13:16 +0300

    Add CustomerBean class and update BusinessObjects main method

--- business-objects/src/main/java/com/lectures/business/objects/BusinessObjects.java
+++ business-objects/src/main/java/com/lectures/business/objects/BusinessObjects.java
@@ -4,6 +4,8 @@
 
 package com.lectures.business.objects;
 
+import com.lectures.business.objects.domain.CustomerBean;
+
 /**
  *
  * @author burak
@@ -11,6 +13,12 @@ package com.lectures.business.objects;
 public class BusinessObjects {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        CustomerBean customer=new CustomerBean(); // creates invalid customer with default constructor
+        // example rule: Company name should not be empty or null
+        System.out.println(customer.getCompanyName());
+        // Every setter should be validated according to the business rules
+        // Invalid customer ID examples according to the business rules
+        customer.setCustomerId("");
+        customer.setCustomerId("TOO LONG ID");
     }
 }
